d4cef21e092c6a859b41cbd0a941a713ea66454b
kilgette <kilgette@gmail.com>
2023-04-12 18:59:14 -0500

    initial outline

---
+++ script.js
@@ -0,0 +1,8 @@
+//function for starting the quiz
+//function for the next question
+//function for checking answer against the api
+//function for score
+//function for time remaining
+
+
+//store high score in local storage
