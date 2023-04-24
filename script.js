edf6f049e19d98f41ef41df5d5fc048094de8585
kilgette <kilgette@gmail.com>
2023-04-23 21:01:34 -0500

    flushed out new structure

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //global variables
+
+
+
+//array of objects detailing questions, options and answers
+const questionArray = [
+  {
+    question: "What is Gonzo's favorite animal:",
+    options: ["goldfish","cows,","cats,"chickens"],
+    answer: "chickens"},
+    
+]
+
 //function for starting the quiz
-//function for the next question
-//function for checking answer against the api
-//function for score
-//function for time remaining
+
+//function for starting the timer
+function timer(){
+  var countdown = setInterval
+}
+
+//function to display the next question 
+
+//function for checking answer against the array 
+
+//function runs if correct answer
+
+//function runs if incorrect answer
 
 
-//store high score in local storage
+//display score 
+
+
