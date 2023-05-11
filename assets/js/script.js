9ef002c8cc0aa06266fad7e5a2dd632cf595bf1a
kilgette <kilgette@gmail.com>
2023-05-10 19:12:29 -0500

    reverted JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,9 @@
 //use the DOM API to select an HTML element by ID
 var startBtn = document.querySelector('#start-button');
 var questionsDisplay = document.querySelector('#questionsDisplay');
-var questionsQuestion = document.querySelector('#questionsQuestion');
+var questionsQuestions = document.querySelector('#questionsQuestion');
 var questionsButtons = document.querySelector('#questionsButtons');
-var questionOptionsEl = document.querySelector('#questionOptions');
 var quizStart = document.querySelector('#quizStart');
-var score = 0; // variable to store the score
-
 //current question
 var currQuestion = 0;
 
@@ -22,81 +19,72 @@ const questionArray = [
     question: "What instrument does Animal play?",
     options: ["drums", "trombone", "guitar", "piano"],
     answer: "drums"
-  },
+  }, 
   {
     question: "In The Great Muppet Caper, Kermit and Fozzie Bear have what relationship?",
     options: ["twins", "rival thieves", "law-enforcement partners", "father and son"],
     answer: "twins"
-  },
+  }, 
   {
     question: "Balcony hecklers Waldorf and Statler are named for what New York City fixtures?",
     options: ["parks", "stadiums", "hotels", "streets"],
     answer: "hotels"
-  },
+  }, 
   {
     question: "Before starring on The Muppet Show, which Muppet first appeared in commercials for Purina?",
     options: ["Gonzo", "Big Bird", "Rowlf", "Elmo"],
     answer: "Rowlf"
-  },
+  }, 
 ]
 
-function startTimer() {
-  var secondsLeft = 60; // set the initial number of seconds to count down from
-  var countdown = setInterval(function () {
-    secondsLeft--; // decrement the secondsLeft variable
-    if (secondsLeft <= 0) {
-      clearInterval(countdown); // clear the interval once the timer reaches 0
-      displayScore(); // display the final score once the timer reaches 0
-    } else {
-      // display the remaining seconds in the HTML
-      document.querySelector('#timerDisplay').textContent = "Time left: " + secondsLeft + " seconds";
-    }
-  }, 1000); // run the interval every second
+//function for starting the timer
+function timer(){
+  var countdown = setInterval
 }
-
 //display score 
 function displayScore() {
-  questionsDisplay.style.display = "none";
-  var displayScore = document.createElement("div");
-  score = Math.round((score / questionArray.length) * 100); // calculate the final score
-  displayScore.textContent = "Your score is: " + score + "%";
-  quizStart.appendChild(displayScore);
-}
 
+}
 
 function startTheQuiz() {
-  quizStart.style.display = "none"
-  questionsDisplay.style.display = "block"
-  displayQuestion();
-  startTimer();
+  quizStart.style.display="none"
+  questionsDisplay.style.display="block"
+  displayQuestion()
+
 }
-
-
 function handleOptionClick() {
-  var currentQuestionObject = questionArray[currQuestion];
-  if (this.textContent === currentQuestionObject.answer) {
-    score++;
-  } else {
-    var secondsLeft = parseInt(document.querySelector('#timer').textContent.split(" ")[2]);
-    secondsLeft -= 10;
-    document.querySelector('#timer').textContent = "Time left: " + secondsLeft + " seconds";
-  }
-  currQuestion++;
-  if (currQuestion < questionArray.length) {
-    displayQuestion();
-  } else {
-    displayScore();
-  }
+  currQuestion++
+  displayQuestion ()
 }
+
 function displayQuestion() {
   var currentQuestionObject = questionArray[currQuestion];
-  questionsQuestion.textContent = currentQuestionObject.question;
-  questionOptionsEl.innerHTML = ""; // Clear the options
-  for (var i = 0; i < currentQuestionObject.options.length; i++) {
-    var option = document.createElement("li");
-    option.className = "option-button";
-    option.textContent = currentQuestionObject.options[i];
-    option.addEventListener("click", handleOptionClick);
-    questionOptionsEl.appendChild(option);
+  console.log(currentQuestionObject)
+
+  questionsQuestions.textContent = currentQuestionObject.question;
+  questionsButtons.innerHTML=""
+  
+  // let's use a for loop to do something repetitively
+  for(var i = 0; i < currentQuestionObject.options.length; i = i + 1){
+    console.log('i is', i);
+    var li = document.createElement('li');
+       li.textContent = questionArray[currQuestion].options[i];
+       li.addEventListener("click" , handleOptionClick); 
+    questionsButtons.append(li);
   }
 }
+
+function gradeTheUsersChoice(event) {
+  console.log('event.target is', event.target);
+  if(event.target.matches('li')) {
+    // grade user's selection once li is clicked 
+    console.log('target\'s textContent is', event.target.textContent);
+  }
+  // now I can compare the li's text to the current question's "answer" property
+}
+
+
+// event listeners
+startBtn.addEventListener('click', startTheQuiz);
+questionChoicesEl.addEventListener('click', gradeTheUsersChoice);
+
